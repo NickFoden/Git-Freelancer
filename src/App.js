415f9e267e0ee562355ac2d47fe77815a2183c57
Nick Foden <nickfoden@gmail.com>
2018-10-18 19:10:20 +0900

    Hard coded fetch works need to parse input and submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gitLog: "",
+      gitRepo: ""
+    };
+  }
+  handleSubmit() {
+    // fetch(`${this.state.gitRepo}`)
+    fetch("https://api.github.com/repos/NickFoden/Git-Freelancer/commits")
+      .then(function(response) {
+        console.log(response);
+        return response.json();
+        // const res = response.json();
+        // console.log(res);
+        // this.setState({
+        //   gitLog: res
+        // });
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+  }
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={() => this.handleSubmit()}>
+          <input
+            type="text"
+            name="gitRepo"
+            onChange={this.handleChange}
+            placeholder="https://github.com/OctoCat/cardboardBoxRepo"
+          />
+          <br />
+          <button type="button" onClick={this.handleSubmit}>
+            Get the Logs
+          </button>
+        </form>
+        <p> {this.state.gitLog}</p>
       </div>
     );
   }
